a828ed1514ff763f2ad7f65ffec8cc45a5d8d1ab
[dyad] <xakatosh66@gmail.com>
2026-04-14 16:54:33 -0300

    [dyad] Corrigindo erro de importação do Avatar na Navbar - wrote 1 file(s)

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -25,6 +25,7 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { useAuth } from './AuthProvider';
 import NotificationBell from './NotificationBell';
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import {
   Sheet,
   SheetContent,
